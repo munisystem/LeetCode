78658c8f842a71fecd02725794e2fd059bfbe65f
Yuichi Saito <munisystem@gmail.com>
2020-12-22 15:36:13 +0900

    Improve longest-common-prefix

--- longest-common-prefix/main.go
+++ longest-common-prefix/main.go
@@ -3,34 +3,31 @@ package main
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
+	} else if len(strs[0]) == 0 {
+		return ""
 	}
-	r, p, i, isBreak := "", "", 1, false
-	for {
-		for _, str := range strs {
-			if len(str) == 0 {
-				isBreak = true
-				break
-			}
-			if p == "" {
-				if i > len(str) {
-					isBreak = true
-					break
-				}
-				p = str[0:i]
-			}
-			if len(p) > len(str) {
-				isBreak = true
-				break
-			}
-			if p != str[0:i] {
-				isBreak = true
-				break
-			}
+
+	str := strs[0]
+	head, tail := 1, len(str)
+	for head <= tail {
+		mid := (head + tail) / 2
+		if isPrefixMatch(strs, str[0:mid]) {
+			head = mid + 1
+		} else {
+			tail = mid - 1
 		}
-		if isBreak {
-			break
-		}
-		r, p, i = p, "", i+1
 	}
-	return r
+	return str[0 : (head+tail)/2]
+}
+
+func isPrefixMatch(strs []string, subst string) bool {
+	for _, str := range strs {
+		if len(subst) > len(str) {
+			return false
+		}
+		if subst != str[0:len(subst)] {
+			return false
+		}
+	}
+	return true
 }
