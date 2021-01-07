6e92c575f0c15e15954737347ea107be936f8380
Yuichi Saito <munisystem@gmail.com>
2021-01-07 13:02:43 +0900

    Use Floyd's cycle-finding algorithm

--- happy-number/main.go
+++ happy-number/main.go
@@ -1,18 +1,23 @@
 package main
 
-func isHappy(n int) bool {
-	for {
-		if n/10 == 0 {
-			if n == 1 || n == 7 {
-				return true
-			} else {
-				return false
-			}
-		}
-		r := 0
-		for x := n; x > 0; x = x / 10 {
-			r = r + (x%10)*(x%10)
-		}
-		n = r
+// Floyd's cycle-finding algorithm
+// c.f. https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_tortoise_and_hare
+func sumDigits(n int) int {
+	r := 0
+	for x := n; x > 0; x = x / 10 {
+		r = r + (x%10)*(x%10)
 	}
+	return r
+}
+
+func isHappy(n int) bool {
+	fast, slow := n, n
+	for slow != 1 {
+		fast = sumDigits(sumDigits(fast))
+		slow = sumDigits(slow)
+		if slow != 1 && slow == fast {
+			return false
+		}
+	}
+	return true
 }
