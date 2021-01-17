5d83eb975e5d9857872a20f7727e0a34a750f24f
Yuichi Saito <munisystem@gmail.com>
2021-01-17 18:29:43 +0900

    Solve construct-binary-tree-from-preorder-and-inorder-traversal

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal/main.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	rootVal := preorder[0]
+	root := &TreeNode{Val: rootVal}
+	i := 0
+	for ; inorder[i] != rootVal; i++ {
+	}
+	root.Left = buildTree(preorder[1:i+1], inorder[0:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
