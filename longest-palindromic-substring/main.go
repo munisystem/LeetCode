c92ab93fb9fa74cca0a6929cd206759360133c65
Yuichi Saito <munisystem@gmail.com>
2021-01-20 11:13:05 +0900

    Re: Solve longest-palindromic-substring

--- longest-palindromic-substring/main.go
+++ longest-palindromic-substring/main.go
@@ -1,28 +1,30 @@
 package main
 
 func longestPalindrome(s string) string {
-	if len(s) <= 1 {
-		return s
-	}
-	res := ""
-	for i := range s {
-		for j := i; j < len(s); j++ {
-			buf := s[i : j+1]
-			if len(buf) == 1 && len(buf) > len(res) {
-				res = buf
-				continue
-			}
-			isPalindrome := true
-			for k := 0; k < len(buf)/2; k++ {
-				if buf[k:k+1] != buf[len(buf)-1-k:len(buf)-k] {
-					isPalindrome = false
-					break
-				}
-			}
-			if isPalindrome && len(buf) > len(res) {
-				res = buf
-			}
+	ans := ""
+	for i := 0; i < len(s); i++ {
+		odd, even := palindrome(s, i, i), palindrome(s, i, i+1)
+		if len(ans) >= len(odd) && len(ans) >= len(even) {
+			continue
+		}
+		if len(odd) > len(even) {
+			ans = odd
+		} else {
+			ans = even
 		}
 	}
-	return res
+	return ans
+}
+
+func palindrome(s string, i, j int) string {
+	str := ""
+	for i >= 0 && j < len(s) {
+		if s[i] != s[j] {
+			break
+		}
+		str = s[i : j+1]
+		i--
+		j++
+	}
+	return str
 }
