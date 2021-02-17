c844796125e87c692745eed7571dbf4f875a8df6
Yuichi Saito <munisystem@gmail.com>
2021-02-17 21:52:36 +0900

    Re: Solve container-with-most-water

--- container-with-most-water/main.go
+++ container-with-most-water/main.go
@@ -1,26 +1,18 @@
 package main
 
+import "math"
+
 func maxArea(height []int) int {
-	if len(height) <= 1 {
-		return 0
-	}
-	max := 0
 	i, j := 0, len(height)-1
-	for {
-		if i == j {
-			break
-		}
-		area := 0
-		if height[i] < height[j] {
-			area = height[i] * (j - i)
-			i = i + 1
+	ans := 0
+	for i < j {
+		h := int(math.Min(float64(height[i]), float64(height[j])))
+		ans = int(math.Max(float64(ans), float64(h*(j-i))))
+		if height[i] > height[j] {
+			j--
 		} else {
-			area = height[j] * (j - i)
-			j = j - 1
-		}
-		if area > max {
-			max = area
+			i++
 		}
 	}
-	return max
+	return ans
 }
