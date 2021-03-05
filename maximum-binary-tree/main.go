785a849e40658d5f734ed9608663ae1750c39d67
Yuichi Saito <munisystem@gmail.com>
2021-03-05 09:57:42 +0900

    Tweak

--- maximum-binary-tree/main.go
+++ maximum-binary-tree/main.go
@@ -9,10 +9,7 @@ type TreeNode struct {
 func constructMaximumBinaryTree(nums []int) *TreeNode {
 	if len(nums) == 0 {
 		return nil
-	} else if len(nums) == 1 {
-		return &TreeNode{Val: nums[0]}
 	}
-	tree := &TreeNode{}
 	mi, mv := 0, nums[0]
 	for i := 1; i < len(nums); i++ {
 		if nums[i] > mv {
@@ -20,8 +17,9 @@ func constructMaximumBinaryTree(nums []int) *TreeNode {
 			mi = i
 		}
 	}
-	tree.Val = mv
-	tree.Left = constructMaximumBinaryTree(nums[0:mi])
-	tree.Right = constructMaximumBinaryTree(nums[mi+1:])
-	return tree
+	return &TreeNode{
+		Val:   nums[mi],
+		Left:  constructMaximumBinaryTree(nums[0:mi]),
+		Right: constructMaximumBinaryTree(nums[mi+1:]),
+	}
 }
