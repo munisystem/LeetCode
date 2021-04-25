70b452d889e3e5db47a2b2527782a961ab929c74
Yuichi Saito <munisystem@gmail.com>
2021-04-25 19:04:33 +0900

    Solve april-leetcoding-challenge-2021/week-4-april-22nd-april-28th/3720

---
+++ april-leetcoding-challenge-2021/week-4-april-22nd-april-28th/3720/main.go
@@ -0,0 +1,14 @@
+package main
+
+func rotate(matrix [][]int) {
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix)/2; j++ {
+			matrix[i][j], matrix[i][len(matrix)-j-1] = matrix[i][len(matrix)-j-1], matrix[i][j]
+		}
+	}
+}
