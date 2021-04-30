5a2d7fa47ab53ee6d8e6dee0560ed0b57f597ca7
Yuichi Saito <munisystem@gmail.com>
2021-04-30 21:48:41 +0900

    Solve 0217-contains-duplicate

---
+++ 0217-contains-duplicate/main.go
@@ -0,0 +1,12 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	m := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]]++
+		if m[nums[i]] > 1 {
+			return true
+		}
+	}
+	return false
+}
