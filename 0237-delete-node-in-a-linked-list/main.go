a754268fae17a4ffc878775eeda928f9575e6b65
Yuichi Saito <munisystem@gmail.com>
2021-05-08 22:51:55 +0900

    Solve 0237-delete-node-in-a-linked-list

---
+++ 0237-delete-node-in-a-linked-list/main.go
@@ -0,0 +1,15 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	for node.Next.Next != nil {
+		node.Val = node.Next.Val
+		node = node.Next
+	}
+	node.Val = node.Next.Val
+	node.Next = nil
+}
