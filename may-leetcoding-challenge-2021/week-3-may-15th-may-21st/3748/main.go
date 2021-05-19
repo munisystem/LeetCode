787186e6bc0e674e2b8033ea5cc1814d46343b21
Yuichi Saito <munisystem@gmail.com>
2021-05-19 23:03:33 +0900

    Solve may-leetcoding-challenge-2021/week-3-may-15th-may-21st/3748

---
+++ may-leetcoding-challenge-2021/week-3-may-15th-may-21st/3748/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func minMoves2(nums []int) int {
+	sort.Ints(nums)
+	i, j := 0, len(nums)-1
+	ans := 0
+	for i < j {
+		ans += nums[j] - nums[i]
+		j--
+		i++
+	}
+	return ans
+}
