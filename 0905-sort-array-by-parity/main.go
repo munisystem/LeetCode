323c002774f200c915b7ccd7cc49558b5c812778
Yuichi Saito <munisystem@gmail.com>
2021-07-29 23:42:16 +0900

    Solve 0905-sort-array-by-parity

---
+++ 0905-sort-array-by-parity/main.go
@@ -0,0 +1,19 @@
+package main
+
+func sortArrayByParity(nums []int) []int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		if nums[left]%2 == 0 {
+			left++
+			continue
+		}
+		if nums[right]%2 != 0 {
+			right--
+			continue
+		}
+		nums[left], nums[right] = nums[right], nums[left]
+		left++
+		right--
+	}
+	return nums
+}
