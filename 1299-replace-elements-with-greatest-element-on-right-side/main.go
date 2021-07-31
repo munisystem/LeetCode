a75c5dfba8a2fe7a7bd03b47bde7c78098b9b7e2
Yuichi Saito <munisystem@gmail.com>
2021-07-31 20:06:17 +0900

    Solve 299-replace-elements-with-greatest-element-on-right-side

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/main.go
@@ -0,0 +1,13 @@
+package main
+
+func replaceElements(arr []int) []int {
+	ans := make([]int, len(arr), len(arr))
+	max := -1
+	for i := len(arr) - 1; i >= 0; i-- {
+		ans[i] = max
+		if max < arr[i] {
+			max = arr[i]
+		}
+	}
+	return ans
+}
