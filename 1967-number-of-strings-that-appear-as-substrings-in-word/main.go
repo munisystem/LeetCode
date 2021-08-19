b648d2bf2df748177303612047ddb983fa7215ac
Yuichi Saito <munisystem@gmail.com>
2021-08-19 23:12:26 +0900

    Tweak

--- 1967-number-of-strings-that-appear-as-substrings-in-word/main.go
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/main.go
@@ -1,19 +1,12 @@
 package main
 
+import "strings"
+
 func numOfStrings(patterns []string, word string) int {
 	var ans int
 	for _, pattern := range patterns {
-		for i := 0; i < len(word)-len(pattern)+1; i++ {
-			var sub string
-			if i+len(pattern) == len(word) {
-				sub = word[i:]
-			} else {
-				sub = word[i : i+len(pattern)]
-			}
-			if pattern == sub {
-				ans++
-				break
-			}
+		if strings.Contains(word, pattern) {
+			ans++
 		}
 	}
 	return ans
