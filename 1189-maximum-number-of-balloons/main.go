8b1a0374c4f1b687685b7406545cfa303b9a96fe
Yuichi Saito <munisystem@gmail.com>
2021-08-20 22:57:29 +0900

    Solve 1189-maximum-number-of-balloons

---
+++ 1189-maximum-number-of-balloons/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func maxNumberOfBalloons(text string) int {
+	chars := make([]int, 5, 5)
+	for _, r := range text {
+		switch r {
+		case 'b':
+			chars[0]++
+		case 'a':
+			chars[1]++
+		case 'l':
+			chars[2]++
+		case 'o':
+			chars[3]++
+		case 'n':
+			chars[4]++
+		}
+	}
+	chars[2] /= 2
+	chars[3] /= 2
+	sort.Ints(chars)
+	return chars[0]
+}
