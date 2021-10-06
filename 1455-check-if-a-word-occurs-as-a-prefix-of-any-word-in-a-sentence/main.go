2f39a94207d5cf84a39a8d88eeffc038a094c3d5
Yuichi Saito <munisystem@gmail.com>
2021-10-06 17:57:29 +0900

    Solve 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "strings"
+
+func isPrefixOfWord(sentence string, searchWord string) int {
+	words := strings.Split(sentence, " ")
+	for i, word := range words {
+		if strings.HasPrefix(word, searchWord) {
+			return i + 1
+		}
+	}
+	return -1
+}
